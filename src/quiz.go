9ce02814267d65828f7a52cfb178605426da870a
Mounica1291 <psai@seas.upenn.edu>
2019-03-06 15:06:27 -0800

    linting modifications

--- src/quiz.go
+++ src/quiz.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"sort"
-	"os"
-	"fmt"
-	"log"
-	"encoding/csv"
 	"bufio"
-	"time"
+	"encoding/csv"
+	"fmt"
 	"io"
+	"log"
+	"os"
+	"sort"
 	"sync"
+	"time"
+
 	"github.com/urfave/cli"
 )
 
 type Quiz struct {
 	Question string
-	Answer string
+	Answer   string
 }
 
 func checkError(e error) {
@@ -24,101 +25,101 @@ func checkError(e error) {
 	}
 }
 
-func main(){
-		//filepath string
-		var filepath string
-		var totaltime int
-		var random bool
-		var points int
-		//this it to fire off a goroutine that keeps track of all goroutines and close gracefully after they end. 
-		var wg sync.WaitGroup
+func main() {
+	//filepath string
+	var filepath string
+	var totaltime int
+	var random bool
+	var points int
+	//this it to fire off a goroutine that keeps track of all goroutines and close gracefully after they end.
+	var wg sync.WaitGroup
 
-		//cli to parse filepath from user. Has a default location to load from incase of no input
-		app := cli.NewApp()
-		app.Name = "Quiz"
-		app.Usage = "Fun Quiz: A Mind Bender"
-	
-		//global level flags 
-		app.Flags = []cli.Flag{
-			cli.StringFlag{
-				Name:        "filepath",
-				Value:       "../data/problems.csv",
-				Usage:       "filepath to load the csv file",
-				Destination: &filepath,
-			},
-			cli.IntFlag{
-				Name: "totaltime",
-				Value: 5,
-				Usage: "Set total time of Quiz",
-				Destination: &totaltime,
-			},
-			cli.BoolFlag{
-				Name: "random",
-				Usage: "Ask questions in random order",
-				Destination: &random,
-			},
-		}
+	//cli to parse filepath from user. Has a default location to load from incase of no input
+	app := cli.NewApp()
+	app.Name = "Quiz"
+	app.Usage = "Fun Quiz: A Mind Bender"
 
-		app.Action = func(c *cli.Context) error {
-			fmt.Println("Welcome to the Quiz")
-			return nil
-		}
+	//global level flags
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "filepath",
+			Value:       "../data/problems.csv",
+			Usage:       "filepath to load the csv file",
+			Destination: &filepath,
+		},
+		cli.IntFlag{
+			Name:        "totaltime",
+			Value:       5,
+			Usage:       "Set total time of Quiz",
+			Destination: &totaltime,
+		},
+		cli.BoolFlag{
+			Name:        "random",
+			Usage:       "Ask questions in random order",
+			Destination: &random,
+		},
+	}
 
-		sort.Sort(cli.FlagsByName(app.Flags))
-		err := app.Run(os.Args)
-		if err != nil {
-			log.Fatal(err)
-		}
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Welcome to the Quiz")
+		return nil
+	}
 
-		Quiz, err := parseCsv(filepath)
-		checkError(err)
-		
-		//wait for Enter to start quiz
-		fmt.Print("Press 'Enter' to continue...")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
+	sort.Sort(cli.FlagsByName(app.Flags))
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		//creating a channel to communicate 
-		communicate := make(chan string)
-		//TODO:implement context 
-		/*
+	Quiz, err := parseCsv(filepath)
+	checkError(err)
+
+	//wait for Enter to start quiz
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	//creating a channel to communicate
+	communicate := make(chan string)
+	//TODO:implement context
+	/*
 		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(totaltime)*time.Second)
-		defer cancel() //this is to make sure all paths cancel the context to avoid context leak 
-		*/
+		defer cancel() //this is to make sure all paths cancel the context to avoid context leak
+	*/
 
-		wg.Add(1)
-		timeout := time.After(time.Duration(totaltime)*time.Minute)
-		//starting go routines for quiz and userinput 
-		go func(){
-		wrap:
-			for i :=0;i < len(Quiz); i++ {
-				go userInput(os.Stdout, os.Stdin, Quiz[i].Question, communicate)
-				select {
-				case <-timeout:
-					fmt.Fprintf(os.Stderr, "Time's up!")
+	wg.Add(1)
+	timeout := time.After(time.Duration(totaltime) * time.Minute)
+	//starting go routines for quiz and userinput
+	go func() {
+	wrap:
+		for i := 0; i < len(Quiz); i++ {
+			go userInput(os.Stdout, os.Stdin, Quiz[i].Question, communicate)
+			select {
+			case <-timeout:
+				fmt.Fprintf(os.Stderr, "Time's up!")
+				break wrap
+
+			case input, ok := <-communicate:
+				if !ok {
 					break wrap
-				
-				case input, ok := <- communicate:
-					if !ok {
-						break wrap
-					}
-					if Quiz[i].Answer == input {
-						points++
-					}
-
 				}
-			}
-			wg.Done()
-		}()
-		wg.Wait()
-		close(communicate)
+				if Quiz[i].Answer == input {
+					points++
+				}
 
-		fmt.Fprintf(os.Stdout, "%d correctly answered out of %d \n", points, len(Quiz))
-		
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	close(communicate)
+
+	fmt.Fprintf(os.Stdout, "%d correctly answered out of %d \n", points, len(Quiz))
+
 }
 
-func userInput(w io.Writer, r io.Reader, question string, sendTo chan string){
+func userInput(w io.Writer, r io.Reader, question string, sendTo chan string) {
 	reader := bufio.NewReader(r)
-	fmt.Fprintln(w, "Question :" + question)
+	fmt.Fprintln(w, "Question :"+question)
 	fmt.Fprint(w, "Answer: ")
 
 	input, err := reader.ReadString('\n')
@@ -133,7 +134,7 @@ func userInput(w io.Writer, r io.Reader, question string, sendTo chan string){
 	sendTo <- input
 }
 
-func parseCsv(filepath string)([]Quiz, error){
+func parseCsv(filepath string) ([]Quiz, error) {
 	//load the csv file
 	f, err := os.Open(filepath)
 	checkError(err)
@@ -160,7 +161,7 @@ func parseCsv(filepath string)([]Quiz, error){
 
 		output = append(output, Quiz{
 			Question: line[0],
-			Answer: line[1],
+			Answer:   line[1],
 		})
 	}
 	return output, nil
